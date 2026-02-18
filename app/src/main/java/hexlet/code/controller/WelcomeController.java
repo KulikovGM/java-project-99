3e1a2987e914032792800a65933a135cc3b44172
Grigoriy <kulikovGM@gmail.com>
2026-02-18 22:29:08 +0300

    Add WelcomeController

---
+++ app/src/main/java/hexlet/code/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package hexlet.code.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("")
+public class WelcomeController {
+    @GetMapping("/welcome")
+    String welcom() {
+        return "welcome";
+    }
+}
