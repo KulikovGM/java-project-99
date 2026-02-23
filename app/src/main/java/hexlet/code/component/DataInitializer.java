1be0c326afea8bd237ebb673d5f9c674fdc3f660
Grigoriy <kulikovGM@gmail.com>
2026-02-23 19:22:22 +0300

    Create private and public keys

--- app/src/main/java/hexlet/code/component/DataInitializer.java
+++ app/src/main/java/hexlet/code/component/DataInitializer.java
@@ -24,7 +24,7 @@ public class DataInitializer implements ApplicationRunner {
         userData.setEmail(email);
         userData.setPasswordDigest("qwerty");
         userData.setFirstName("admin");
-        userData.setLastName("adminov");
+        userData.setLastName("admin");
         userRepository.save(userData);
     }
 }
