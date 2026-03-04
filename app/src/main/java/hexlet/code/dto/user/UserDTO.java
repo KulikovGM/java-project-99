744f0034e23e7ccd596d78957836ee713130c818
Grigoriy <kulikovGM@gmail.com>
2026-03-04 20:21:44 +0300

    Update UserDTO

--- app/src/main/java/hexlet/code/dto/user/UserDTO.java
+++ app/src/main/java/hexlet/code/dto/user/UserDTO.java
@@ -12,7 +12,5 @@ public class UserDTO {
     private String email;
     private String firstName;
     private String lastName;
-    private String password;
     private LocalDate createdAt;
-//    private LocalDate updatedAt;
 }
