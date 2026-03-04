9e03d3a32150c7c71af7292e015c204b6e63c4a6
Grigoriy <kulikovGM@gmail.com>
2026-03-04 22:36:30 +0300

    Update

--- app/src/main/java/hexlet/code/component/DataInitializer.java
+++ app/src/main/java/hexlet/code/component/DataInitializer.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
 
+@SuppressWarnings("java:S6437")
 @Component
 @AllArgsConstructor
 public class DataInitializer implements ApplicationRunner {
